4f6608d10ba5bd176e5c7e788815f85070270ac5
Nedinator <imnedinator@gmail.com>
2024-02-20 10:21:39 -0600

    Added tags to Referer type to prep that feature.

--- data/data.go
+++ data/data.go
@@ -7,24 +7,25 @@ import (
 )
 
 type Url struct {
-	ShortUrl  string    `json:"shorturl"`
-	ShortId   string    `json:"shortid"`
-	LongUrl   string    `json:"longurl"`
-	Clicks    int       `json:"clicks"`
-	CreatedAt time.Time `json:"createdat"`
-	Owner     string    `json:"owner"`
-	Referer   []Referer `json:"referer"`
+	ShortUrl  string    `json:"shorturl" bson:"shorturl"`
+	ShortId   string    `json:"shortid" bson:"shortid"`
+	LongUrl   string    `json:"longurl" bson:"longurl"`
+	Clicks    int       `json:"clicks" bson:"clicks"`
+	CreatedAt time.Time `json:"createdat" bson:"createdat"`
+	Owner     string    `json:"owner" bson:"owner"`
+	Referer   []Referer `json:"referer" bson:"referer"`
 }
 
 type Referer struct {
-	Domain string `json:"domain" bson:"domain"`
-	Clicks int    `json:"clicks" bson:"clicks"`
+	Domain string   `json:"domain" bson:"domain"`
+	Clicks int      `json:"clicks" bson:"clicks"`
+	Tags   []string `json:"tags" bson:"tags"`
 }
 
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID       string `json:"id" bson:"id"`
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
 }
 
 func AuthData(c *fiber.Ctx) fiber.Map {
